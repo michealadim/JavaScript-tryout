5afa5bc290c8b48afcd588a7c2168e5d412da7b6
Ekene Adim <michealadim@users.noreply.github.com>
2020-03-27 10:42:11 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = "Ekene";
+console.log("My name is: " + name);
+
+let course = "HTML, JavaScript, Css";
+console.log("My courses are: " + course);
+
+
+const data = [];
+
+for(let i=0; i<=200; i++) {
+	if(i%3==0) {
+
+		data.push(i);
+
+	}
+}
+
+console.log(data);
